31363126a6bd26b15ab1ae841ea51292ce553df7
Aarsh Malik <aarshmalik35@gmail.com>
2026-07-23 15:30:52 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://irrigation-project-production.up.railway.app"
+    baseURL: "http://irrigation.eba-whnpsfmm.us-east-1.elasticbeanstalk.com",
 });
 
 export default api;
